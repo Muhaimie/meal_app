23fe9919be3d20687cfc8345dfecb7e1f5e3195c
Muhaimie Mazlah <laws_basic@yahoo.co.uk>
2020-05-20 21:44:28 +0800

    Screen navigation & routing system

---
+++ lib/dummy_data.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+
+const DUMMY_CATEGORIES =  const [
+
+Category(id: "c1", title: "Italian", color: Colors.purple),
+Category(id: "c2", title: "Quick & easy", color: Colors.blue),
+Category(id: "c3", title: "Hamburger", color: Colors.pink),
+Category(id: "c4", title: "Sphagetti", color: Colors.red)
+];
